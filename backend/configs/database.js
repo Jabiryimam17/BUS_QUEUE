a504a62fc3df1b0333d5ff576e205f2deff5bd78
Jabir Yimam <jabiryimam1@gmail.com>
2026-04-18 23:24:24 +0300

    avian trial 2

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -4,8 +4,7 @@ dotenv.config();
 let pool = null;
 const create_pool = () => {
   if (!pool) {
-    
-
+    console.log(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME, process.env.DB_PORT, process.env.DB_POOL_MAX, process.env.DB_POOL_IDLE_TIMEOUT_MS, process.env.DB_POOL_CONNECTION_TIMEOUT_MS);
     pool = new Pool({
       host: process.env.DB_HOST || 'localhost',
       user: process.env.DB_USER || 'root',
